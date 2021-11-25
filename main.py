c28174dbf1a6d886815fbb636625ce3c215fe870
Belen <mbelenclerici@gmail.com>
2021-11-25 02:18:56 -0300

    modifique rama principal

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 print("Bienvenidos")
 print("Fin")
-<<<<<<< HEAD
 print("FFFFFFFF")
-=======
-print("se agregaron lineas de codigo")
->>>>>>> ac321a5c1adae6aaa38cd53999996f821419347d
+print(*****)
+
+
+
+print("chauuuuu")
