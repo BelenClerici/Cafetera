4c1ff3feb9f299fbf35e828bf1a72a98d2b31905
“BelenC” <mbelenclerici@gmail.com>
2021-11-29 12:26:34 -0300

    se creo funcion vaciar cafetera

--- cafe.py
+++ cafe.py
@@ -4,8 +4,15 @@ class Cafetera:
         self.cant_actual =0
     
     def llenar_cafetera(self):
-    self.cap_max= self.cant_actual
+        self.cap_max= self.cant_actual
 
-    def servir_taza()=
+    def agregar_cafe(self,cantidad):
+        if self.can_actual + cantidad > self.cap_max:
+            raise ValueError (" La cantidad total supera la capacidad máxima" )
+            self.cant_actual = self.cant_actual + cantidad 
+        else:
+            self.cant_actual = 0 
     
-    
+    def vaciar_cafetera():
+        self.cant_actual=0
+        
